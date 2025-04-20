d6c55af026e35c07cba1fb66e708f79bc7f97ae0
JhommuelPastoral <jhommuelpastoral9@gmail.com>
2025-04-20 10:20:37 +0800

    Update userContext.jsx

--- frontend/context/userContext.jsx
+++ frontend/context/userContext.jsx
@@ -10,7 +10,7 @@ export default function UserContextProvider({ children }) {
 
   useEffect(() => {
     // Call the profile endpoint
-    axios.get("http://localhost:5000/api/users/profile")
+    axios.get("https://testbackend-lqgk.onrender.com/api/users/profile")
       .then(({ data }) => {
         // If there's a valid user, set it
         if (data) {
